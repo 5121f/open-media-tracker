a52d5bf5e3656bfb57dc45a7c1bc6f1094b89803
Artyom Kalin <kalin.a.v20031@gmail.com>
2024-04-20 14:11:27 +0300

    make serial list scrollable

--- src/screen/main.rs
+++ src/screen/main.rs
@@ -1,5 +1,5 @@
 use iced::{
-    widget::{column, horizontal_space, row, text},
+    widget::{column, horizontal_space, row, scrollable, text},
     Alignment, Element,
 };
 
@@ -27,20 +27,22 @@ impl MainScreen {
     pub fn view(&self) -> Element<Message> {
         column![
             square_button("+").on_press(Message::AddSerial),
-            column(
-                self.media
-                    .iter()
-                    .enumerate()
-                    .map(|(id, m)| row![
-                        text(&m.name),
-                        horizontal_space(),
-                        square_button("...").on_press(Message::ChangeSerial(id))
-                    ]
-                    .align_items(Alignment::Center))
-                    .map(Into::into)
-                    .collect::<Vec<_>>()
+            scrollable(
+                column(
+                    self.media
+                        .iter()
+                        .enumerate()
+                        .map(|(id, m)| row![
+                            text(&m.name),
+                            horizontal_space(),
+                            square_button("...").on_press(Message::ChangeSerial(id))
+                        ]
+                        .align_items(Alignment::Center))
+                        .map(Into::into)
+                        .collect::<Vec<_>>()
+                )
+                .spacing(DEFAULT_INDENT)
             )
-            .spacing(DEFAULT_INDENT)
         ]
         .spacing(DEFAULT_INDENT)
         .padding(DEFAULT_INDENT)
