8300abe04b36a1d74a03642710668799947593a1
Artyom Kalin <kalin.a.v20031@gmail.com>
2024-10-14 02:13:05 +0300

    remove AsRef implementation for Icon

--- src/gui/icon.rs
+++ src/gui/icon.rs
@@ -18,17 +18,11 @@ impl Icon {
     }
 }
 
-impl AsRef<char> for Icon {
-    fn as_ref(&self) -> &char {
-        &self.0
-    }
-}
-
 pub fn text<'a, Message>(icon: Icon) -> Text<'a, Message>
 where
     Message: iced::widget::text::Catalog + 'a,
 {
-    Text::new(icon.as_ref()).font(ICON_FONT)
+    Text::new(icon.0).font(ICON_FONT)
 }
 
 pub fn button<'a, Message>(icon: Icon) -> Button<'a, Message> {
