6ebce9bf7a6f726a3db7a848b31730ae76ee3157
Artyom Kalin <kalin.a.v20031@gmail.com>
2025-03-03 06:06:24 +0300

    improve API

--- src/model/media.rs
+++ src/model/media.rs
@@ -39,10 +39,10 @@ impl Media {
         }
     }
 
-    pub async fn read(path: &Path) -> Result<Self> {
+    pub async fn read(path: impl AsRef<Path>) -> Result<Self> {
         let file_content = async_fs::read_to_string(&path).await?;
         let media = ron::from_str(&file_content)
-            .map_err(|source| ErrorKind::deserialize(path.to_owned(), source))?;
+            .map_err(|source| ErrorKind::deserialize(path.as_ref().to_owned(), source))?;
         Ok(media)
     }
 
