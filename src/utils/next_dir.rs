48b9d9db5110a8726189c89eadd7e00771e8305e
Artyom Kalin <kalin.a.v20031@gmail.com>
2025-08-22 11:45:53 +0300

    avoid clonning

--- src/utils/next_dir.rs
+++ src/utils/next_dir.rs
@@ -30,6 +30,6 @@ pub async fn next_dir(path: impl AsRef<Path>) -> Result<PathBuf> {
     if next_chapter_index >= paths.len() {
         return Err(ErrorKind::find_next_chapter(path));
     }
-    let next_dir = paths[next_chapter_index + 1].clone();
+    let next_dir = paths.swap_remove(next_chapter_index + 1);
     Ok(next_dir)
 }
